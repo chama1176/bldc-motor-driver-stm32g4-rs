cb1e6cb63107c4bb0256422f4bba65d38ba91bc7
kaede <kaede6120@gmail.com>
2022-03-21 12:12:11 +0900

    Hello stm32g4

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![deny(warnings)]
+#![deny(unsafe_code)]
+#![no_std]
+#![no_main]
+
+extern crate cortex_m;
+extern crate cortex_m_rt as rt;
+extern crate panic_semihosting;
+extern crate stm32g4xx_hal as hal;
+
+use cortex_m_semihosting::hprintln;
+use rt::entry;
+
+#[entry]
+fn main() -> ! {
+    hprintln!("Hello, STM32G4!").unwrap();
+
+    #[allow(clippy::empty_loop)]
+    loop {}
+}
