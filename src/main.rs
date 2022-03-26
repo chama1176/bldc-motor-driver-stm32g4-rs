8dd21912d28caa2d14ba3dbaf081e1bb58bca654
kaede <kaede6120@gmail.com>
2022-03-26 15:13:12 +0900

    double led

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,7 @@ mod g4test {
             
         }
     }
+
     impl<'a> Led0<'a> {
         pub fn new(perip: &'a Peripherals) -> Self {
             // Peripheralがいろんなところから変更されていいのか???
@@ -54,6 +55,41 @@ mod g4test {
         }
     }
 
+    pub struct Led1<'a> {
+        perip: &'a Peripherals,
+    }
+    
+    impl<'a> Led for Led1<'a> {
+        fn on(&self) {
+            let gpioc = &self.perip.GPIOC;
+            gpioc.bsrr.write(|w| w.bs14().set());
+        }
+        fn off(&self) {
+            let gpioc = &self.perip.GPIOC;
+            gpioc.bsrr.write(|w| w.br14().reset());
+        }
+        fn toggle(&self) {
+            
+        }
+    }
+    impl<'a> Led1<'a> {
+        pub fn new(perip: &'a Peripherals) -> Self {
+            // Peripheralがいろんなところから変更されていいのか???
+            // stm32f401モジュールより、ペリフェラルの入り口となるオブジェクトを取得する。
+            Self{
+                perip,
+            }
+        }
+        pub fn init(&self) {
+            // GPIOCポートの電源投入(クロックの有効化)
+            self.perip.RCC.ahb2enr.modify(|_,w| w.gpiocen().set_bit());
+
+            // gpio初期化(PC13を出力に指定)
+            let gpioc = &self.perip.GPIOC;
+            gpioc.moder.modify(|_,w| w.moder14().output());
+        }
+    }
+
     
 }
 
@@ -67,17 +103,21 @@ fn main() -> ! {
     // hprintln!("Hello, STM32G4!").unwrap();
     let perip = stm32g431::Peripherals::take().unwrap();
     let led0 = g4test::Led0::new(&perip);
+    let led1 = g4test::Led1::new(&perip);
 
     led0.init();
+    led1.init();
     loop {
         loop {
             // hprintln!("Set Led High").unwrap();
             for _ in 0..10_000 {
                 led0.on();
+                led1.off();
             }
             // hprintln!("Set Led Low").unwrap();
             for _ in 0..10_000 {
                 led0.off();
+                led1.on();
             }
         }
     }
