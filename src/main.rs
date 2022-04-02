685b5e3b1947a0ede59e296591f99b22de465acd
kaede <kaede6120@gmail.com>
2022-04-02 17:25:21 +0900

    tmp

--- src/main.rs
+++ src/main.rs
@@ -39,8 +39,6 @@ mod g4test {
 
     impl<'a> Led0<'a> {
         pub fn new(perip: &'a Peripherals) -> Self {
-            // Peripheralがいろんなところから変更されていいのか???
-            // stm32f401モジュールより、ペリフェラルの入り口となるオブジェクトを取得する。
             Self{
                 perip,
             }
@@ -74,8 +72,6 @@ mod g4test {
     }
     impl<'a> Led1<'a> {
         pub fn new(perip: &'a Peripherals) -> Self {
-            // Peripheralがいろんなところから変更されていいのか???
-            // stm32f401モジュールより、ペリフェラルの入り口となるオブジェクトを取得する。
             Self{
                 perip,
             }
@@ -84,7 +80,7 @@ mod g4test {
             // GPIOCポートの電源投入(クロックの有効化)
             self.perip.RCC.ahb2enr.modify(|_,w| w.gpiocen().set_bit());
 
-            // gpio初期化(PC13を出力に指定)
+            // gpio初期化(PC14を出力に指定)
             let gpioc = &self.perip.GPIOC;
             gpioc.moder.modify(|_,w| w.moder14().output());
         }
@@ -101,6 +97,7 @@ fn main() -> ! {
     use stm32g4::stm32g431;
 
     // hprintln!("Hello, STM32G4!").unwrap();
+    // stm32f401モジュールより、ペリフェラルの入り口となるオブジェクトを取得する。
     let perip = stm32g431::Peripherals::take().unwrap();
     let led0 = g4test::Led0::new(&perip);
     let led1 = g4test::Led1::new(&perip);
