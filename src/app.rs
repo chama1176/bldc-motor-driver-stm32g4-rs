c84e46a9e28627d3a47a3578c92e0917578d2036
kaede <kaede6120@gmail.com>
2023-08-12 23:39:47 +0900

    change app struct

--- src/app.rs
+++ src/app.rs
@@ -2,16 +2,11 @@ use crate::indicator::Indicator;
 use crate::three_phase_motor_driver::ThreePhase;
 use crate::three_phase_motor_driver::ThreePhaseMotorDriver;
 
-pub struct Data {
-    tv: f32,
-    count: u64,
+pub enum State {
+    Waiting,
+    Calibrating,
+    Operating,
 }
-impl Data {
-    pub fn new() -> Self {
-        Self { tv: 0.0, count: 0 }
-    }
-}
-
 pub struct App<T0, T1, T2, M>
 where
     T0: Indicator,
@@ -19,7 +14,9 @@ where
     T2: Indicator,
     M: ThreePhaseMotorDriver,
 {
-    data: Data,
+    tv: f32,
+    count: u64,
+    // 
     led0: T0,
     led1: T1,
     led2: T2,
@@ -35,7 +32,8 @@ where
 {
     pub fn new(led0: T0, led1: T1, led2: T2, bldc: M) -> Self {
         Self {
-            data: Data::new(),
+            tv: 0.0, 
+            count: 0,
             led0,
             led1,
             led2,
@@ -50,7 +48,7 @@ where
 
         let mut tp: ThreePhase<u32> = ThreePhase { u: 0, v: 0, w: 0 };
 
-        match ((self.data.count as f32/(10.0*1.0)) as u64)%6 {
+        match ((self.count as f32/(10.0*1.0)) as u64)%6 {
             0 => tp = ThreePhase{u: 200, v: 0, w: 0},
             1 => tp = ThreePhase{u: 200, v: 200, w: 0},
             2 => tp = ThreePhase{u: 0, v: 200, w: 0},
@@ -59,7 +57,7 @@ where
             5 => tp = ThreePhase{u: 200, v: 0, w: 200},
             6_u64..=u64::MAX => (),
         }
-        if self.data.tv < 0.1 {
+        if self.tv < 0.1 {
             tp = ThreePhase{ u: 0, v: 0, w: 0 };
         }
         self.bldc.set_u_pwm(tp.u);
@@ -67,9 +65,9 @@ where
         self.bldc.set_w_pwm(tp.w);
     }
     pub fn set_target_velocity(&mut self, tv: f32) {
-        self.data.tv = tv;
+        self.tv = tv;
     }
     pub fn set_count(&mut self, c: u64) {
-        self.data.count = c;
+        self.count = c;
     }
 }
